dd9011d5b91f400f73e6bdd8186e29e1102546d5
makinyelure <oakinyelure@hotmail.com>
2019-09-14 08:53:29 -0400

    Created application entry point. No logic should be placed directly inside this class

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,18 +1,12 @@
+import Core.Libraries.Server.AppServer;
 import Core.Route;
 
 import static spark.Spark.awaitInitialization;
-import static spark.Spark.port;
 
 public class Entry {
     public static void main(String[] args) {
-        String portNumber = System.getProperty("PORT");
-        port(8080);
-
-        if(portNumber != null) {
-            port(Integer.parseInt(portNumber));
-        }
+        AppServer.init();
         Route.resolve();
-
         awaitInitialization();
     }
 }
