5340ce0d2a3a76170bc1989215ffecd9ddf5ca80
makinyelure <oakinyelure@hotmail.com>
2019-09-19 01:30:47 -0400

    Added methods to the parser and created possible identifiers

--- src/main/java/Core/EngineModules/Dom/DomParser.java
+++ src/main/java/Core/EngineModules/Dom/DomParser.java
@@ -1,4 +1,25 @@
 package Core.EngineModules.Dom;
 
+import Core.EngineModules.Crawlers.RestCrawl;
+
+import java.net.URISyntaxException;
+import java.util.Map;
+
 public class DomParser {
+
+    private boolean _crawlerActive;
+
+    private boolean _saveMode;
+
+    private RestCrawl _crawler;
+
+    public DomParser() throws URISyntaxException {
+        this._crawler = new RestCrawl();
+    }
+
+    public void parseAsync() {
+        this._crawler.initAsync();
+        Map<String,String> resultSet = this._crawler.getCrawlResults();
+    }
+
 }
