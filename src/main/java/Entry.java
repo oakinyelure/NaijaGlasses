b12f93e8962a30b0007b58ecde9a6b19aa69fb68
makinyelure <oakinyelure@hotmail.com>
2019-09-19 01:28:38 -0400

    Added Dom parser to application entry so that it could run as soon as the server starts

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,10 +1,19 @@
+import Core.EngineModules.Dom.DomParser;
 import Core.Libraries.Server.AppServer;
 import Core.Route;
-
-import static spark.Spark.awaitInitialization;
+import java.net.URISyntaxException;
+import static spark.Spark.*;
 
 public class Entry {
+
     public static void main(String[] args) {
+        try {
+            DomParser parser = new DomParser();
+            parser.parseAsync();
+        }
+        catch (URISyntaxException ex) {
+            // Todo: Would probably send notification to admin that there is an issue with the parser
+        }
         AppServer.init();
         Route.resolve();
         awaitInitialization();
