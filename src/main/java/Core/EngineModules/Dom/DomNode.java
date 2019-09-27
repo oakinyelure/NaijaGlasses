3a95f248e5e6276ab4275eb1f6d7e40944203675
makinyelure <oakinyelure@hotmail.com>
2019-09-26 22:37:48 -0400

    Domnode is the format or a specific node of a crawled content

---
+++ src/main/java/Core/EngineModules/Dom/DomNode.java
@@ -0,0 +1,77 @@
+package Core.EngineModules.Dom;
+
+public class DomNode {
+    private DomNode _parentNode;
+
+    private String _outerLink;
+
+    private String _textContent;
+
+    private String _imageLink;
+
+    private String _url;
+
+    private int _searchCount;
+
+    public DomNode() {
+    }
+
+    public DomNode getParentNode() {
+        return _parentNode;
+    }
+
+    public void setParentNode(DomNode _parentNode) {
+        this._parentNode = _parentNode;
+    }
+
+    public String getOuterLink() {
+        return _outerLink;
+    }
+
+    public void setOuterLink(String _outerLink) {
+        this._outerLink = _outerLink;
+    }
+
+    public String getTextContent() {
+        return _textContent;
+    }
+
+    public String getUrl() {
+        return _url;
+    }
+
+    public void setUrl(String _url) {
+        this._url = _url;
+    }
+
+    public int getSearchCount() {
+        return _searchCount;
+    }
+
+    public void setSearchCount(int _searchCount) {
+        this._searchCount = _searchCount;
+    }
+
+    public void setTextContent(String _textContent) {
+        this._textContent = _textContent;
+    }
+
+    public String getImageLink() {
+        return _imageLink;
+    }
+
+    public void setImageLink(String _imageLink) {
+        this._imageLink = _imageLink;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("href => "+this.getOuterLink());
+        builder.append("img => "+this.getImageLink());
+        builder.append("text => "+this.getTextContent());
+        builder.append("URL => "+this.getUrl());
+        builder.append("Parent => "+((this._parentNode != null) ? this._parentNode.toString() : "null"));
+        return builder.toString();
+    }
+}
