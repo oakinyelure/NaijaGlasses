4a076e29b92093a08cee71eeb78811ab4c97b5bf
makinyelure <oakinyelure@hotmail.com>
2019-10-19 02:08:23 -0400

    Created search capability and ranking. It still need to be refactored to reduce the speed of processing results

--- src/main/java/Core/EngineModules/Search/SearchEngine.java
+++ src/main/java/Core/EngineModules/Search/SearchEngine.java
@@ -6,8 +6,12 @@ import DataContext.Mongo;
 import dev.morphia.Datastore;
 import dev.morphia.query.Query;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
     private Mongo _mongoClient;
@@ -17,9 +21,29 @@ public class SearchEngine {
     }
 
     public List get(String keyword) {
+        List<String> keywordParts = Arrays.asList(keyword.split(" "));
         Datastore data = this._mongoClient.getOrmInstance();
-        List searchResult = new ArrayList();
-        Query<DomIndexedContent> query = data.createQuery(DomIndexedContent.class);
-        return query.asList();
+        ArrayList<DomIndexedContent> webContents = (ArrayList<DomIndexedContent>) data.createQuery(DomIndexedContent.class).find().toList();
+        List<DomIndexedContent> rankedResults = this.rank(keywordParts,webContents);
+        return webContents;
+    }
+
+    private List<DomIndexedContent> rank(List<String> searchEntities, ArrayList<DomIndexedContent> indexedContent) {
+        for(int i = 0; i < indexedContent.size(); i++) {
+            HashSet<String> currentContent = indexedContent.get(i).contents;
+            int currentIndex = i;
+            currentContent.forEach(content -> {
+                List<String> delimitedContent = Arrays.asList(content.split(" "));
+                List<String> commonContent = delimitedContent.stream().filter(searchEntities::contains).collect(Collectors.toList());
+                if(commonContent.size() > 0) {
+                    indexedContent.get(currentIndex).searchHitCount++;
+                }
+            });
+        }
+        // sort in ascending order. Highest search hit first
+        indexedContent.sort((current, next) -> {
+            return next.searchHitCount - current.searchHitCount;
+        });
+        return indexedContent;
     }
 }
