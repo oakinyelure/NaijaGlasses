12bbc4ad9f91a012610088a070af552d35e51eb2
makinyelure <oakinyelure@hotmail.com>
2019-10-19 02:07:39 -0400

    Sanitized string and passed to the search engine class

--- src/main/java/Core/Controllers/SearchController.java
+++ src/main/java/Core/Controllers/SearchController.java
@@ -2,14 +2,18 @@ package Core.Controllers;
 
 
 import Core.EngineModules.Search.SearchEngine;
+import Core.Exceptions.InvalidSearchRequest;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 public class SearchController extends ApiController {
     public SearchController() {}
 
     public Object Search() {
         try {
+            String searchKeyword = Jsoup.clean(this.request.queryParams("q"), Whitelist.simpleText());
             SearchEngine search = new SearchEngine();
-            return search.get("test");
+            return search.get(searchKeyword);
         }
         catch (Exception ex) {
             response.status(500);
